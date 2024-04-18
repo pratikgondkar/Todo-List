98fe8ff427043b97e7b25cc6f1f375af80f765b2
pratikgondkar <pratikgondkar4040@gmail.com>
2024-04-18 22:30:55 +0530

    Added reducer

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,41 +3,48 @@ import TodoContext from "../../context/TodoContext";
 import { useContext } from "react";
 
 function TodoList() {
+
     const { list, setList } = useContext(TodoContext);
+
+    function onFinished(todo, isFinished) {
+        const updatedList = list.map(t => {
+            if(t.id === todo.id) {
+                return { ...t, finished: isFinished }; // Correctly update the finished property of the current todo
+            }
+            return t;
+        });
+        setList(updatedList);
+    }
+
+    function onDelete( todo,  ) {
+        const updatedList = list.filter(t => t.id !== todo.id); 
+        setList(updatedList);
+    }
+
+    function onEdit(todo, todoText) {
+        const updatedList = list.map(t => {
+            if(t.id == todo.id) {
+               return {...t,  todoData: todoText};
+            }
+            return t;
+        });
+        setList(updatedList);
+    }
+    
 
     return (
         <div>
-            {list.length > 0 && list.map(todo => (
-                <Todo 
-                    key={todo.id} 
-                    id={todo.id} 
-                    isFinished={todo.finished} 
-                    todoData={todo.todoData} 
-                    changeFinished={(isFinished) => {
-                        console.log("isFinished", isFinished);
-                        const updatedList = list.map(t => {
-                            if(t.id === todo.id) {
-                                return { ...t, finished: isFinished }; // Correctly update the finished property of the current todo
-                            }
-                            return t;
-                        });
-                        setList(updatedList);
-                    }}
-                    onDelete={() => {
-                        const updatedList = list.filter(t => t.id !== todo.id); // Use !== instead of !=
-                        setList(updatedList);
-                    }}
-                    onEdit={(todoText) => {
-                        const updatedList = list.map(t => {
-                            if(t.id == todo.id) {
-                                todo.todoData = todoText;
-                            }
-                            return t;
-                        });
-                        setList(updatedList);
-                    }}
+            {list.length > 0 && 
+            list.map(todo => <Todo 
+                                    key={todo.id} 
+                                    id={todo.id} 
+                                    isFinished={todo.finished} 
+                                    todoData={todo.todoData} 
+                                    changeFinished={(isFinished) => onFinished(todo, isFinished)}
+                                    onDelete={() => onDelete(todo) }
+                                    onEdit={(todoText) =>onEdit(todo, todoText)}
                 />
-            ))}
+            )}
         </div>
     );
 }
---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,26 @@
+function todoReducer(state, action) {
+    if(action.type == 'add_todo') {
+        let todoText = action.payload.todoText;
+        return [
+            ...state, {id: state.length + 1 , todoData: todoText, finished: false}
+            ]
+
+    } else if (action.type == 'edit_todo') {
+        let todo = action.payload.todo;
+        let todotext = action.payload.todoText;
+        const updatedList = state.map(t => {
+            if(t.id == todo.id) {
+                todo.todoData = todoText;
+            }
+            return t;
+        });
+        return updatedList;
+
+    } else if (action.type == 'delete_todo') {
+        let todo = action.payload.todo;
+        const updatedList = state.filter(t => t.id !== todo.id);
+        return updatedList;
+    }
+}
+
+export default todoReducer;
